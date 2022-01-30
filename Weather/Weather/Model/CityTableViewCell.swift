5152d0e3bbe57243722f5bd52c9cb57b56e172c5
MinyoungKoo <rnalsdud1014@naver.com>
2022-01-30 22:29:29 +0900

    Feat: 변경 테이블뷰 셀
    타입을 명확히 하기 위해 속성 이름을 변경하고, 이미지캐싱 적용을 위해 기존 이미지 설정 메서드 대신 새로 변경한 메서드를 호출한다.

--- Weather/Weather/Model/CityTableViewCell.swift
+++ Weather/Weather/Model/CityTableViewCell.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class CityTableViewCell: UITableViewCell{
-    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     
     func update(_ city: City) {
-        icon.load(image: city.weatherImage)
+        iconImageView.setImage(urlString: city.weatherImageUrl ?? "")
         nameLabel.text = city.name
         tempLabel.text = "현재기온 " + String(city.temp ?? 0) + "도"
         humidityLabel.text = "현재습도 " + String(city.humidity ?? 0) + "%"
