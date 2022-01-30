79fb32c2ed4e20707230ee8e82395f18acac0a9b
MinyoungKoo <rnalsdud1014@naver.com>
2022-01-30 22:23:40 +0900

    Feat: 변경 UIImageView 확장
    화면에 날씨 아이콘 이미지를 빠르게 로딩하기 위해, url로 다운로드한 이미지는 메모리에 저장 해놓고 불러와 사용하는 방식으로 이미지세팅 메서드를 수정한다.

--- Weather/Weather/Extension/UIImageView+.swift
+++ Weather/Weather/Extension/UIImageView+.swift
@@ -9,11 +9,24 @@ import Foundation
 import UIKit
 
 extension UIImageView {
-    func load(image: UIImage?) {
-        guard let image = image else {return}
+    func setImage(urlString: String) {
+        // 저장된(이미 다운로드한) 이미지가 있으면 사용
+        if let cacheImage = CacheManager.shared.object(forKey: urlString as NSString) {
+            self.image = cacheImage
+            return
+        }
+        
+        // 없으면 url로 이미지 다운로드해서 사용
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self,
+                  let url = URL(string: urlString),
+                  let data = try? Data(contentsOf: url),
+                  let image = UIImage(data: data) else {return}
+            
             DispatchQueue.main.async {
+                CacheManager.shared.setObject(image, forKey: urlString as NSString)
                 self.image = image
             }
         }
+    }
 }
-
